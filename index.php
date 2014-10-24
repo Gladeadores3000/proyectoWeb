52b951a31e725d3444224de02287c6c273f5e230
1211agarcia <1211agarcia@gmail.com>
2014-10-24 02:33:14 -0430

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,27 @@
-/*|------------------------------------------|
-      |              CONSULTAR TODOS             |
-      |__________________________________________|*/
-    function consultarTodos($table, $i, $f) {
-        
-        $query ="select * from ".$table ."limit ".$i.",".$f;
-        $datos = ejecutar($query); //mysqli_query($sql,$link) or die (mysql_error());
-        
-        $row=mysql_fetch_array($datos);
-        
-        print_r($row);
-
-        return($row);
-    }
-    /*|------------------------------------------|
-      |                FILTRAR                   |
-      |__________________________________________|*/
-    function filtrar($table) {
-        
-        $query ="select * from ".$table." WHERE ;
-        $datos = ejecutar($query); //mysqli_query($sql,$link) or die (mysql_error());
-        
-        $row=mysql_fetch_array($datos);
-        
-        print_r($row);
-
-        return($row);
-    }
-
-    
-?>
-
-
+<?php
+                        include 'php/opGenericas.php';
+                             // Realizar una consulta MySQL
+                            $resultado = consultarTodos("contenido", 0, 3);
+                            
+                            while ($line = mysqli_fetch_assoc($resultado)) {
+                                printf( "
+                                   <div class=col-md-4 >
+                                        <div class=gallery-item >
+                                            <div class=gallery-thumb>
+                                                <img src=$line[url] alt=$line[titulo]>
+                                                <div class=overlay-p>
+                                                    <a href=#portfolio class=login-window>
+                                                        <i class='fa fa-arrows-alt fa-2x'></i>
+                                                    </a>
+                                                </div>
+                                            </div> 
+                                            <h3 class=gallery-title><a href=#>$line[titulo]</a></h3>
+                                        </div> 
+                                    </div>   
+                                     <div id=portfolio class=login-popup>
+                                    <a href=# class=close><img src=images/close_pop.png class=btn_close title=Close Window alt=Close /></a>        
+                                        <img src=$line[url]>
+                                    </div>
+                                ");
+                            }
+                    ?>
